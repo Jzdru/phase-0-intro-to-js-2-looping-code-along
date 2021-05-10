6685bb962a5744a72979e8932b5e9cb4a2359b22
Joe <joezdru@gmail.com>
2021-05-09 20:16:49 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solutions in this file
+const cards = (["Joe", "Mikelle", "Amanda"], `Birthday`)
+
+function writeCards( cards, event ) {
+    let thankYouCards = []
+    for ( let i = 0; i < cards.length; i++ ) {
+      thankYouCards.push(`Thank you, ${cards[i]}, for the wonderful ${event} gift!`)
+    }
+    return thankYouCards
+  }
+  
+  function countDown(countdown) {
+    while (countdown >= 0) {
+      console.log(countdown);
+      countdown -= 1;
+  function countDown(startingNumber) {
+    while (startingNumber >= 0) {
+      console.log(startingNumber);
+      startingNumber -= 1;
+    }
+    console.log(countdown);
+    console.log(startingNumber);
+}
+
+    }
+
+}
